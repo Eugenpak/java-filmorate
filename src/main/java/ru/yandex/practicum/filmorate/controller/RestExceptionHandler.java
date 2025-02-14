e93145e7d291342ec0a50d85c822ba63cd2f98d9
Eugenpak <techteamit@yandex.ru>
2025-02-14 22:19:23 +0300

    Создать пакет controller: FilmController, UserController, RestExceptionHandler.

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/RestExceptionHandler.java
@@ -0,0 +1,39 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.yandex.practicum.filmorate.exception.ErrorMessage;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+
+@ControllerAdvice
+@Slf4j
+public class RestExceptionHandler {
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ErrorMessage> catchValidationException(ValidationException e) {
+        log.error(e.getMessage(), e);
+        return new ResponseEntity<>(new ErrorMessage(HttpStatus.BAD_REQUEST.value(), e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorMessage> catchNotFoundException(NotFoundException e) {
+        log.error(e.getMessage(), e);
+        return new ResponseEntity<>(new ErrorMessage(HttpStatus.NOT_FOUND.value(), e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(new ErrorMessage(HttpStatus.BAD_REQUEST.value(), e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorMessage> handleUnknownException(Throwable e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
