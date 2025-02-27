aa232a4cee332c2f63373189cda465acb3ac5892
Eugenpak <techteamit@yandex.ru>
2025-02-28 02:01:46 +0300

    Checkstyle Spr-11-v-1

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,9 +6,14 @@ import java.util.Collection;
 
 public interface FilmStorage {
     Collection<Film> findAll();
+
     Film create(Film film);
+
     Film update(Film film);
+
     public Film findFilmById(long id);
+
     public void addLike(long filmId, long userId);
+
     public void deleteLike(long filmId, long userId);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,11 +5,18 @@ import java.util.Collection;
 
 public interface UserStorage {
     Collection<User> findAll();
+
     User create(User user);
+
     User update(User user);
+
     User findUserById(long id);
+
     void addFriend(long userId, long friendId);
+
     void removeFromFriends(long userId, long friendId);
+
     Collection<User> getAllFriends(long userId);
+
     Collection<User> getCommonFriends(long userId,long otherId);
 }
