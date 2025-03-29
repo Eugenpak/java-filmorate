2b3f25cb6727bd6b34d56af5be4d2c65f0172ac4
Eugenpak <techteamit@yandex.ru>
2025-03-29 21:55:08 +0300

    Checkstyle Spr-12-FZ-v-6

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -55,5 +55,7 @@ public class InMemoryFilmStorage implements FilmStorage {
     }
 
     @Override
-    public List<Film> getPopularFilms(int count) { return null;}
+    public List<Film> getPopularFilms(int count) {
+        return null;
+    }
 }
