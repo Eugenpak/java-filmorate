b765d947875d68b77d7947548ca399f5d1dbe041
Eugenpak <techteamit@yandex.ru>
2025-03-29 21:12:27 +0300

    Добавить в пакет main.resources: InternalServerException.

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,67 @@
+
+create table if not exists genres
+(
+    id   BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name varchar(100) not null
+);
+
+create table if not exists mpas
+(
+    id   BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name varchar(100) not null
+);
+
+create table if not exists users
+(
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+	email    varchar(255) not null,
+	login    varchar(255) not null,
+    name     varchar(255),    
+    birthday date         not null    
+);
+
+create table if not exists friends
+(
+    user_id   BIGINT,
+    friend_id BIGINT,
+	confirmed boolean,
+    foreign key (user_id) references users (id),
+    foreign key (friend_id) references users (id),
+    primary key (user_id, friend_id)
+);
+
+create table if not exists films
+(
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name         varchar(255) not null,
+	description  varchar(200),
+    release_date date,    
+    duration     int
+);
+
+create table if not exists likes
+(
+    film_id BIGINT,
+    user_id BIGINT,
+    foreign key (film_id) references films (id) on delete cascade,
+    foreign key (user_id) references users (id) on delete cascade,
+    primary key (film_id, user_id)
+);
+
+create table if not exists film_genres
+(
+    film_id  BIGINT,
+    genre_id BIGINT,
+    foreign key (film_id) references films (id) on delete cascade,
+    foreign key (genre_id) references genres (id) on delete cascade,
+    primary key (film_id, genre_id)
+);
+
+create table if not exists film_mpas
+(
+    film_id BIGINT,
+	mpa_id  BIGINT,
+	foreign key (film_id) references films (id) on delete cascade,
+    foreign key (mpa_id) references mpas (id) on delete cascade,    
+    primary key (film_id, mpa_id)
+);
