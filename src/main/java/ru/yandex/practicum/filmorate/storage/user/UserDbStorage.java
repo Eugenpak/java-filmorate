ea91eca121234f3724527931dc3a4473c941f9a8
Eugenpak <techteamit@yandex.ru>
2025-03-29 22:47:55 +0300

    Checkstyle Spr-12-FZ-v-11

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -12,6 +13,7 @@ import java.util.Optional;
 
 @Repository
 @Qualifier("UserDbStorage")
+@Slf4j
 public class UserDbStorage extends BaseDbStorage<User> implements UserStorage {
     private static final String FIND_ALL_QUERY = "SELECT * FROM users";
     private static final String FIND_BY_ID_QUERY = "SELECT * FROM users WHERE id = ?";
@@ -27,11 +29,13 @@ public class UserDbStorage extends BaseDbStorage<User> implements UserStorage {
 
     @Override
     public Collection<User> findAll() {
+        log.debug("UserDbStorage findAll().");
         return findMany(FIND_ALL_QUERY);
     }
 
     @Override
     public User create(User user) {
+        log.debug("UserDbStorage create(user:{}).", user);
         long id = insert(
                 INSERT_QUERY,
                 user.getEmail(),
@@ -45,6 +49,7 @@ public class UserDbStorage extends BaseDbStorage<User> implements UserStorage {
 
     @Override
     public User update(User user) {
+        log.debug("UserDbStorage update(user:{}).", user);
         update(
                 UPDATE_QUERY,
                 user.getEmail(),
@@ -58,23 +63,41 @@ public class UserDbStorage extends BaseDbStorage<User> implements UserStorage {
 
     @Override
     public Optional<User> findUserById(long id) {
+        log.debug("UserDbStorage findUserById(id:{}).", id);
         return findOne(FIND_BY_ID_QUERY, id);
     }
 
     @Override
     public boolean delUserById(long id) {
+        log.debug("UserDbStorage delUserById(id:{}).", id);
         return delete(DELETE_QUERY, id);
     }
 
     @Override
-    public void addFriend(long userId, long friendId) {}
+    public void addFriend(long userId, long friendId) {
+        log.debug("UserDbStorage addFriend(userId:{}, friendId:{}).", userId, friendId);
+        // Смотри friendStorage.addFriend(userId,friendId);
+        log.trace("У пользователя ID_{} добавлен друг с ID_{}.", userId, friendId);
+    }
 
     @Override
-    public void removeFromFriends(long userId, long friendId) {}
+    public void removeFromFriends(long userId, long friendId) {
+        log.debug("UserDbStorage removeFromFriends(userId:{}, friendId:{}).", userId, friendId);
+        // Смотри friendStorage.removeFromFriends(userId,friendId);
+        log.trace("У пользователя ID_{} удалён друг с ID_{}.", userId, friendId);
+    }
 
     @Override
-    public Collection<User> getAllFriends(long userId) {return null;}
+    public Collection<User> getAllFriends(long userId) {
+        log.debug("UserDbStorage getAllFriends(userId:{}).", userId);
+        // Смотри friendStorage.getFriendsAll(userId);
+        return null;
+    }
 
     @Override
-    public Collection<User> getCommonFriends(long userId,long otherId) {return null;}
+    public Collection<User> getCommonFriends(long userId,long otherId) {
+        log.debug("UserDbStorage getCommonFriends(userId:{}, otherId:{}).", userId, otherId);
+        // Смотри friendStorage.getFriendsCommon(userId,otherId);
+        return null;
+    }
 }
