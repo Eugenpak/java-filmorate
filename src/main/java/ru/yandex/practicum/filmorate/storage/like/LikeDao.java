6fa07bf138d9e388196b3a43c865c4fedaf8a4af
itsmekatrine <itsmekatrine1@gmail.com>
2025-04-11 12:24:37 +0300

    Расширение DAO для работы с лайками

--- src/main/java/ru/yandex/practicum/filmorate/storage/like/LikeDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/like/LikeDao.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.storage.like;
 
 import ru.yandex.practicum.filmorate.model.PopularFilm;
 import java.util.Collection;
+import java.util.Set;
 
 public interface LikeDao {
     void add(long filmId, long userId);
@@ -11,4 +12,8 @@ public interface LikeDao {
     Collection<PopularFilm> findPopularFilmsId(int count);
 
     void deleteAllPopularFilms();
+
+    Set<Long> getLikedFilmsIdsByUser(long userId);
+
+    Set<Long> getUserIdsByLikedFilm(long filmId);
 }
