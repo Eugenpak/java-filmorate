27185ddbeaef34c122211ec55d9ec14fc3a610c4
Eugenpak <techteamit@yandex.ru>
2025-04-14 20:04:02 +0300

    Checkstyle Spr-13-add-reviews-v01

--- src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequestMapping("/reviews")
 public class ReviewController {
     private final ReviewService reviewService;
+
     @Autowired
     public ReviewController(ReviewService reviewService) {
         this.reviewService = reviewService;
