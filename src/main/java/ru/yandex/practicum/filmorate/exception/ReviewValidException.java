1d77b87ff819d0e4010414658c32ed43adc5bf3a
Eugenpak <techteamit@yandex.ru>
2025-04-14 19:52:10 +0300

    Сохранить пакет exception.

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ReviewValidException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.exception;
+
+public class ReviewValidException extends RuntimeException {
+    public ReviewValidException(String message) {
+        super(message);
+    }
+}
