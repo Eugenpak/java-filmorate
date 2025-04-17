2ae4351a6b90ec3dc5cd124d6bdf23d512e223af
Eugenpak <techteamit@yandex.ru>
2025-04-18 00:27:52 +0300

    Сохранить изменения тестов: FilmControllerTest, FilmControllerWebMvcTest.

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -4,27 +4,33 @@ import jakarta.validation.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
 import java.time.LocalDate;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 class FilmControllerTest {
     private Film film;
 
-    /*@Mock
+    @Mock
     private FilmStorage filmStorage;
     @Mock
     private UserService userService;
 
     @InjectMocks
-    private FilmService filmService;*/
+    private FilmService filmService;
 
     void validateInput(Film film) throws ConstraintViolationException {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
@@ -54,136 +60,41 @@ class FilmControllerTest {
 
     @Test
     void shouldNotNullNameValidation() {
-        /*film.setName(null);
+        film.setName(null);
         try {
             validateInput(film);
         } catch (ConstraintViolationException ex) {
             assertEquals(ex.getMessage(),"name: Имя фильма обязательно");
-        }*/
+        }
     }
 
     @Test
     void shouldNotBlankNameValidation() {
-        /*film.setName("   ");
+        film.setName("   ");
         try {
             validateInput(film);
         } catch (ConstraintViolationException ex) {
             assertEquals(ex.getMessage(),"name: Имя фильма обязательно");
-        }*/
+        }
     }
 
     @Test
     void shouldNotPassMax200DescriptionValidation() {
-        /*film.setDescription("Матрица New ".repeat(500));
+        film.setDescription("Матрица New ".repeat(500));
         try {
             validateInput(film);
         } catch (ConstraintViolationException ex) {
             assertEquals(ex.getMessage(),"description: Описание фильма должно содержать до 200 символов");
-        }*/
-    }
-
-    //@Test
-    void shouldNotPassReleaseDateValidation() {
-        /*
-        LocalDate releaseFilm = LocalDate.of(0,1,1);
-        film.setId(null);
-        film.setReleaseDate(releaseFilm);
-        User user = getTestUser();
-        when(filmStorage.create(film)).thenReturn(film);
-        when(userService.findUserById(1)).thenReturn(user);
-
-
-        //UserStorage userStorage = new InMemoryUserStorage();
-        //FriendStorage friendStorage = new FriendDbStorage()
-        //UserService userService = new UserService(userStorage);
-        //FilmStorage filmStorage = new InMemoryFilmStorage();
-        FilmService filmService = new FilmService(filmStorage,userService);
-
-        FilmController fc = new FilmController(filmService);
-        try {
-           fc.create(film);
-        } catch (ValidationException ex) {
-            assertEquals(ex.getMessage(),"Дата релиза — не раньше 28 декабря 1895 года.");
         }
-
-         */
     }
 
     @Test
     void shouldNotNegativeDurationValidation() {
-        /*film.setDuration(-10);
+        film.setDuration(-10);
         try {
             validateInput(film);
         } catch (ConstraintViolationException ex) {
             assertEquals(ex.getMessage(),"duration: Продолжительность фильма >= 0");
-        }*/
-    }
-
-    //@Test
-    void findAll() {
-        /*
-        UserStorage userStorage = new InMemoryUserStorage();
-        UserService userService = new UserService(userStorage);
-        FilmStorage filmStorage = new InMemoryFilmStorage();
-        FilmService filmService = new FilmService(filmStorage,userService);
-
-        FilmController fc = new FilmController(filmService);
-
-        film.setId(null);
-        fc.create(film);
-
-        assertEquals(1,fc.findAll().size());
-        assertEquals(1,fc.findAll().stream().toList().get(0).getId());
-        */
-
-    }
-
-    //@Test
-    void createFilm() {
-        /*
-        UserStorage userStorage = new InMemoryUserStorage();
-        UserService userService = new UserService(userStorage);
-        FilmStorage filmStorage = new InMemoryFilmStorage();
-        FilmService filmService = new FilmService(filmStorage,userService);
-
-        FilmController fc = new FilmController(filmService);
-
-        film.setId(null);
-        Film createdFilm = fc.create(film);
-        assertEquals(1,createdFilm.getId());
-        assertEquals("name film",createdFilm.getName());
-        assertEquals("description film",createdFilm.getDescription());
-        assertEquals(120,createdFilm.getDuration());
-        */
-    }
-
-    //@Test
-    void updateFilm() {
-        /*
-        UserStorage userStorage = new InMemoryUserStorage();
-        UserService userService = new UserService(userStorage);
-        FilmStorage filmStorage = new InMemoryFilmStorage();
-        FilmService filmService = new FilmService(filmStorage,userService);
-
-        FilmController fc = new FilmController(filmService);
-
-        film.setId(null);
-        Film savedFilm = fc.create(film);
-
-        Film modifiedfilm = Film.builder()
-                .id(1L)
-                .name("UPDATE film")
-                .description("UPDATE film")
-                .releaseDate(new Date(0))
-                .duration(200)
-                .build();
-        final Film updateFilm = fc.update(modifiedfilm);
-        assertEquals(updateFilm,savedFilm);
-        assertEquals(1,updateFilm.getId());
-        assertEquals("UPDATE film",updateFilm.getName());
-        assertEquals("UPDATE film",updateFilm.getDescription());
-        assertEquals(200,updateFilm.getDuration());
-        assertEquals(1,fc.findAll().size());
-        */
+        }
     }
 }
---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerWebMvcTest.java
@@ -0,0 +1,87 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.service.FilmService;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FilmController.class)
+public class FilmControllerWebMvcTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private FilmService testFilmService;
+
+    private List<Film> getFilms() {
+        Film one = new Film(1L, "name-1", "description-1", LocalDate.of(2000,7,1),100,
+                new HashSet<>(getGenres()),new Mpa(1L,"G"),
+                List.of(new Director(1L,"director-1")));
+        Film two = new Film(2L, "name-2", "description-2", LocalDate.of(19780,7,1),100,
+                new HashSet<>(getGenres()),new Mpa(2L,"PG"),
+                List.of(new Director(2L,"director-2")));
+        return List.of(one, two);
+    }
+
+    private List<Genre> getGenres() {
+        // Mpa: (4,'R'), (5,'NC-17');
+        Genre one = new Genre(1L, "Комедия");
+        Genre two = new Genre(2L, "Драма");
+        return List.of(one, two);
+    }
+
+    @Test
+    void findAllShouldReturnAllFilms() throws Exception {
+        Mockito.when(this.testFilmService.findAll()).thenReturn(getFilms());
+
+        mvc.perform(get("/films").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2));
+    }
+
+    @Test
+    void findFilmByIdShouldReturnValidFilm() throws Exception {
+        Mockito.when(this.testFilmService.findFilmById(1L)).thenReturn(getFilms().get(0));
+
+        mvc.perform(get("/films/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.name").value("name-1"));
+    }
+
+    @Test
+    void createFilmShouldReturnValidFilm() throws Exception {
+        /*
+        Mockito.when(testFilmService.create(getFilms().get(0))).thenReturn(getFilms().get(0));
+
+        String jsonString = "{\"name\":\"name-1\",\"description\":\"description-1\",\"releaseDate\":\"2000-07-01\"," +
+                "\"duration\":100,\"genres\":[{\"id\":1},{\"id\":2}],\"mpa\":{\"id\":1},\"directors\":[{\"id\":1}]}";
+        //{"name":"New film","releaseDate":"1999-04-30","description":"New film about friends","duration":120,"rate":4,"mpa":{"id":3},"genres":[{"id":1}]}
+
+        mvc.perform(post("/films").content(jsonString)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("name-1"))
+                .andExpect(jsonPath("$.description").value("description-1"));
+
+         */
+    }
+}
